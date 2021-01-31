27b86210123394d562b543a5e9403d61dfe53ed1
Julia Butenhoff <cockatoo242@gmail.com>
2021-01-31 17:59:35 -0600

    Song Folder Directory Created

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public InputField directory;
+    public InputField songTitle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +24,31 @@ public class MainMenu : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void CreateSongFiles()
+    {
+        try
+        {
+            directory.text = directory.text.Trim();
+
+            if (directory.text.EndsWith("\\"))
+            {
+                Debug.Log(directory.text + songTitle.text);
+                PlayerPrefs.SetString("SongDir", directory.text + songTitle.text);
+            } 
+            else
+            {
+                Debug.Log(directory.text + "\\" + songTitle.text);
+                PlayerPrefs.SetString("SongDir", directory.text + "\\" + songTitle.text);
+            }
+
+            // Create the new working dir
+            Directory.CreateDirectory(PlayerPrefs.GetString("SongDir"));
+        } 
+        catch (IOException ex)
+        {
+            Debug.Log(ex.Message);
+        }
     }
 }
